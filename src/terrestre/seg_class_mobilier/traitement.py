9126b0286476594d03c949f0bdebe370fad82369
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2022-11-09 11:39:26 +0100

    suppression fichiers de config pour n'en garder qu'un

--- src/terrestre/seg_class_mobilier/traitement.py
+++ src/terrestre/seg_class_mobilier/traitement.py
@@ -20,6 +20,10 @@ def add_OriginId():
 
 
 def ground_above_ground_segmentation():
+    CONFIG['input'] = CONFIG['output']
+    CONFIG['pipeline'] = "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/pipelines" \
+                         "/Seg_sol_sursol.json "
+
     os.system("pdal-parallelizer process-pipelines -c "
               "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/config_output.json -it dir -nw 3 "
               "-tpw 1")
@@ -30,35 +34,48 @@ def above_ground_segmentation():
               "D:/data_dev/street_pointcloud_process/output/output.las -DENSITY 0.3 -TYPE KNN -C_EXPORT_FMT LAS "
               "-SAVE_CLOUDS FILE D:/data_dev/street_pointcloud_process/output/output.las")
 
+    CONFIG['input'] = "D:/data_dev/street_pointcloud_process/output/output.las"
+    CONFIG['pipeline'] = "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/pipelines/seg_sursol.json"
+
     os.system("pdal-parallelizer process-pipelines -c "
               "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/config_sursol.json -it single -ts "
               "1000 1000 -nw 3 -tpw 1")
 
 
 def mobile_objects_classification():
+    CONFIG['input'] = CONFIG['output']
+    CONFIG['pipeline'] = "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/pipelines" \
+                         "/classification_objets_mobiles.json "
+
     os.system("pdal-parallelizer process-pipelines -c "
               "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/config_obj_mobiles.json -it dir "
               "-nw 3 -tpw 1")
 
 
 def calculate_scattering_anisotropy():
+    CONFIG['pipeline'] = "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/pipelines" \
+                         "/calcul_scattering_anisotropy.json "
+
     os.system("pdal-parallelizer process-pipelines -c "
               "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/config_anisotropy.json -it dir -nw"
               " 3 -tpw 1")
 
 
 def above_ground_classification():
+    CONFIG['pipeline'] = "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/pipelines" \
+                         "/classification_sursol.json "
+
     os.system("pdal-parallelizer process-pipelines -c "
               "D:/calba/street_pointcloud_process/src/terrestre/seg_class_mobilier/config_class_sursol.json -it dir "
               "-nw 3 -tpw 1")
 
 
 if __name__ == "__main__":
-    # add_OriginId()
-    # ground_above_ground_segmentation()
-    # merge()
-    # above_ground_segmentation()
-    # mobile_objects_classification()
-    # calculate_scattering_anisotropy()
-    # above_ground_classification()
+    add_OriginId()
+    ground_above_ground_segmentation()
+    merge()
+    above_ground_segmentation()
+    mobile_objects_classification()
+    calculate_scattering_anisotropy()
+    above_ground_classification()
     merge()
